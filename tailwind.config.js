a3676041214b881637d9ac305e592da7027c711a
winona ivana <winonaivana@gmail.com>
2023-06-06 17:06:08 +0100

    add media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "gray-20": "#F8F4EB",
+        "gray-20": "#F5F5F5",
         "gray-50": "#EFE6E6",
         "gray-100": "#DFCCCC",
         "gray-500": "#5E0000",
-        "primary-100": "#FFE1E0",
+        "primary-100": "#000000",
         "primary-300": "#FFA6A3",
         "primary-500": "#FF6B66",
         "secondary-400": "#FFCD5B",
